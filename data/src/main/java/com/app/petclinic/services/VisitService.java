0b4fe7eaf4076000f40fededa3be867aef519614
mrmadwolf <mdshoykotislam05@gmail.com>
2022-09-07 21:18:22 +0000

    implemented visit service with map. Closes #12

---
+++ data/src/main/java/com/app/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.app.petclinic.services;
+
+import com.app.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+    
+}
---
+++ data/src/main/java/com/app/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,40 @@
+package com.app.petclinic.services.map;
+
+import java.util.Set;
+
+import com.app.petclinic.model.Visit;
+import com.app.petclinic.services.VisitService;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService{
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if (object.getPet() == null || object.getPet().getId() == null || object.getPet().getOwner() == null || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(object);
+    }
+    
+}
