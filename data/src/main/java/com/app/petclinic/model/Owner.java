ef3ffa1cfa258974605fc101aefd48b67cd5061d
mrmadwolf <mdshoykotislam05@gmail.com>
2022-09-07 05:27:37 +0000

    modified Owner as an entity. Closes create Visit entity #6

--- data/src/main/java/com/app/petclinic/model/Owner.java
+++ data/src/main/java/com/app/petclinic/model/Owner.java
@@ -2,8 +2,25 @@ package com.app.petclinic.model;
 
 import java.util.*;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
-    private String address, city, telephone;
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "telephones")
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     
