e540e1de611a514c6e4bbf5e50c081c1f0e5cd43
mrdevsam <mdshoykotislam05@gmail.com>
2022-11-10 13:26:34 +0600

    task completed. closes #30

--- web/src/main/java/com/app/petclinic/controllers/vetController.java
+++ web/src/main/java/com/app/petclinic/controllers/vetController.java
@@ -1,26 +1,33 @@
 package com.app.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.app.petclinic.model.Vet;
 import com.app.petclinic.services.VetService;
 
 @Controller
 public class vetController {
-    
+
     private final VetService vetService;
 
-    
     public vetController(VetService vetService) {
         this.vetService = vetService;
     }
 
-
-    @RequestMapping({"vets", "/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @GetMapping({ "vets", "/vets", "/vets/index", "/vets/index.html", "/vets.html" })
     public String vetIndex(Model model) {
-        
+
         model.addAttribute("vets", vetService.findAll());
-        return "vets/index.html";
+        return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
